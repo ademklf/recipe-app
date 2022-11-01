0faade1b032795c5160f9875cd842faa1379ce45
adem kalfalı <ademkalfa9@gmail.com>
2022-11-01 15:27:47 +0300

    Router_End

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -3,11 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRouter = () => {
   const user = true;
-  return (
-    <div>
-      user? <Outlet /> : <Navigate to="/login" />
-    </div>
-  );
+  return <div>{user ? <Outlet /> : <Navigate to="/login" />}</div>;
 };
 
 export default PrivateRouter;
