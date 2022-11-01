9c9df1460a3541f3e9f28d6f6bdbc2059068afd7
adem kalfalı <ademkalfa9@gmail.com>
2022-11-01 15:02:20 +0300

    Router_Added

--- src/components/globalStyles/GlobalStyles.jsx
+++ src/components/globalStyles/GlobalStyles.jsx
@@ -1,1 +1,15 @@
 import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+*{
+    margin: 0;
+    pad: 0;
+    box-sizing: border-box;
+    font-family: "Girassol, cursive";
+}
+body{
+    font-size: 1.5rem;
+    
+}
+
+`;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GlobalStyles } from "../components/globalStyles/GlobalStyles";
 import Navbar from "./../components/nav/Navbar";
+import Home from "./../pages/home/Home";
+import Login from "../pages/login/Login";
+import About from "./../pages/about/About";
+import Detail from "./../pages/detail/Detail";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       <Navbar />
       <Routes>
-        <Route />
+        <Route path="" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="about" element={<About />} />
+        <Route path="/" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
