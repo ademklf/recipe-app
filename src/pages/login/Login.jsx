23febd148d32650622e946d1ce66077882358664
adem kalfalı <ademkalfa9@gmail.com>
2022-11-02 14:33:02 +0300

    Login_end

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,40 @@
 import React from "react";
-import { FormContainer, LoginContainer } from "./Login.style";
+import {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+} from "./Login.style";
+import { useNavigate } from "react-router-dom";
+import meal from "../../assets/meal.svg";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const userInfo = {
+    username: "admin",
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("user", JSON.stringify(userInfo));
+    navigate("-1");
+  };
+
   return (
     <LoginContainer>
-      <FormContainer></FormContainer>
+      <FormContainer>
+        <StyledImg src={meal} />
+        <Header>{"<CW/>"}Recipe</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput placeholder="Enter username" type={"text"} />
+          <StyledInput placeholder="Password" type={"password"} />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
     </LoginContainer>
   );
 };
