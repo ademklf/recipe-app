2ed7071b7e85eeb67bb74193ede203197b948d1b
adem kalfalı <ademkalfa9@gmail.com>
2022-11-03 13:36:43 +0300

    Navbar_start

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.style";
+import Nav, { Brand } from "./Navbar.style";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Nav justify="space-between" wrap="wrap">
+      <Brand to="/">
+        <i>{"<Clarusway/>"}</i>
+        <span>Recipe</span>
+      </Brand>
+
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="about">About</Link>
+        <Link to="register">Register</Link>
+        <Link to="Logout">Logout</Link>
+      </div>
+    </Nav>
+  );
 };
 
 export default Navbar;
