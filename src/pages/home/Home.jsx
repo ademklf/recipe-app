31d791d2b28aeece5021f815368abf02ecf672d8
adem kalfalı <ademkalfa9@gmail.com>
2022-11-05 15:18:20 +0300

    Recipe-App

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+  const APP_ID = "7e6a6c65";
+  const APP_KEY = "90c58a4680f9f2b586938756169dc24e";
+  const [query, setQuery] = useState("");
+  const [selectedMeal, setSelectedMeal] = useState("");
 
-export default Home
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+  return <div>Home</div>;
+};
+
+export default Home;
